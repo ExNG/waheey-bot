c2b5ca5862f234b1a4da0e91290ac743c1f6c887
ExNG <24767736+ExNG@users.noreply.github.com>
2017-08-28 15:49:31 +0200

    Added mentiones
    All recent attempts to tweet "Waheey" failed because of duplicates

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-#!/usr/bin/env python3
+#!/usr/bin/python
 
+import config
+import random
 import twitter
 
-api = twitter.Api(consumer_key='',
-                  consumer_secret='',
-                  access_token_key='',
-                  access_token_secret='')
+api = twitter.Api(consumer_key=config.consumer_key,
+                  consumer_secret=config.consumer_secret,
+                  access_token_key=config.access_token_key,
+                  access_token_secret=config.access_token_secret)
 
-status = api.PostUpdate('Waheey!')
+Status = "Waheey!"
+fall = random.randint(0,3)
+
+if fall == 0:
+	Status = "Waheey!"
+elif fall == 1:
+	Status = "Waheey! @ectplvzm"
+elif fall == 2:
+	Status = "Waheey! @CubeDachs"
+elif fall == 3:
+	Status = "Waheey! @ExNGBLU"
+
+status = api.PostUpdate(Status)
 print(status.text)
