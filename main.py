c865029447356a971d759f884bdfb5d9ad90536e
Johann Behr <johannbehr@exng.de>
2018-01-02 15:44:06 +0100

    Set the new twitter tweet limit

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ api = twitter.Api(consumer_key=config.consumer_key,
                   access_token_secret=config.access_token_secret)
 
 # generate string with a random amound of e's
-es = "e" * random.randint(1,130)
+es = "e" * random.randint(1, 270)
 
 # create the tweet string
 # there's a small chance of the string being capitalized
-if random.randint(0,1) == 1:
+if random.randint(0, 1) == 1:
     Status = "Wah".upper() + es.upper() + "y!".upper()
 else:
     Status = "Wah" + es + "y!"
